c17a4e3b9998e0f0213a3d96a56f8f916267035e
Thomas Pendleton <thomas6576@yahoo.com>
2022-11-19 22:44:28 -0800

    add sign in component

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,40 @@ import ImageLinkForm from "./components/ImageLinkForm"
 import FaceRecognition from "./components/FaceRecognition"
 import Logo from "./components/Logo"
 import Rank from "./components/Rank"
-
-
-
+import SignIn from "./components/SignIn"
 
 function App() {
-  const [input, setInput] = useState('')
-
+  const [input, setInput] = useState("")
+  const [route, setRoute] = useState("signIn")
 
   const onInputChange = (e) => {
-    console.log(e.target.value);
+    console.log(e.target.value)
   }
 
   const onSubmit = () => {
-    console.log('click');
+    console.log("click")
   }
 
   return (
     <div className="App">
-      <ParticlesBg className='particle' type="cobweb" bg={true} color="#777777" />
+      <ParticlesBg
+        className="particle"
+        type="cobweb"
+        bg={true}
+        color="#777777"
+      />
+
       <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
-      <FaceRecognition />            
+      {route === "signIn" ? (
+        <SignIn />
+      ) : (
+        <>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
+          <FaceRecognition />
+        </>
+      )}
     </div>
   )
 }

2bc8ff30a171d0b039bb5473761c0254eff43410
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-02 23:17:18 -0800

    add clarifi

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Register from "./components/Register"
 
 function App() {
   const [input, setInput] = useState("")
+  const [imageUrl, setImageUrl] = useState('')
   const [route, setRoute] = useState("signIn")
   const [box, setBox] = useState({})
   const [isSignedIn, setIsSignedIn] = useState(false)
@@ -58,9 +59,42 @@ function App() {
     console.log(e.target.value)
   }
 
-  const onSubmit = () => {
-    console.log("click")
-  }
+  // const onSubmit = () => {
+  //   console.log("click")
+  //   setImageUrl(input);
+  //   app.models
+  //     .predict(
+  //   // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  //   // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+  //   // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+  //   // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+  //   // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+  //   // so you would change from:
+  //   // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  //   // to:
+  //   // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+  //       Clarifai.FACE_DETECT_MODEL,
+  //       input)
+  //     .then(response => {
+  //       console.log('hi', response)
+  //       if (response) {
+  //         fetch('http://localhost:3000/image', {
+  //           method: 'put',
+  //           headers: {'Content-Type': 'application/json'},
+  //           body: JSON.stringify({
+  //             id: user.id
+  //           })
+  //         })
+  //           .then(response => response.json())
+  //           .then(count => {
+  //             this.setState(Object.assign(this.state.user, { entries: count}))
+  //           })
+
+  //       }
+  //       this.displayFaceBox(this.calculateFaceLocation(response))
+  //     })
+  //     .catch(err => console.log(err));
+  // }
 
   const onRouteChange = (route) => {
     if (route === "signout") {

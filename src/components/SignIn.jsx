539145a2a5f69f3f970719ef0a8bd37059ff45e6
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-07 15:03:44 -0800

    test netlify ci

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -39,7 +39,7 @@ const SignIn = ({ onRouteChange, setSignIn, loadUser }) => {
       <main className="pa4 black-80">
         <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Sign Me In</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Email
